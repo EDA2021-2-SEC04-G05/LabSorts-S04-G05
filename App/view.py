f647324541b8213a72f7b595c17cb75cfdc081f7
jean rojas <jerojassantos@gmail.com>
2021-09-10 12:17:50 -0500

    modificaciones input5

--- App/view.py
+++ App/view.py
@@ -20,12 +20,15 @@
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
  """
 
+from types import SimpleNamespace
 import config as cf
 import sys
 import controller
 from DISClib.ADT import list as lt
 assert cf
 
+defauly_limit=1000
+sys.setrecursionlimit(defauly_limit*100)
 
 """
 La vista se encarga de la interacción con el usuario
@@ -83,10 +86,17 @@ def printBestBooks(books):
 
 def printSortResults(ord_books, sample=10):
     # TODO completar modificaciones para el laboratorio 4
-    pass
+    size= lt.size(ord_books)
+    if size > sample:
+     print("los primeros ", sample,"libros ordenados son:")
+     i=1
+     while i <= sample:
+         book= lt.getElement(ord_books,i)
+         print('Titulo:' + book['title']+' ISBN:'+ book['isbn']+ ' Rating:'+ book['average_rating'])
+         i+1
+
 
 catalog = None
-
 """
 Menu principal
 """
@@ -123,7 +133,8 @@ while True:
         size = input("Indique tamaño de la muestra: ")
         result = controller.sortBooks(catalog, int(size))
         print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
-                                          str(result))
+                                          str(result[0]))
+        printSortResults(result[1])
 
     else:
         sys.exit(0)
